3a81c7ca8fc4e44316b24ca2c3f0144ec78a5ccd
unknown <purvi.harniya@gmail.com>
2021-05-01 11:06:09 +0530

    login pg 1

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <!-- <iframe src="https://assets.pinterest.com/ext/embed.html?id=859765385106571877" height="336" width="236" frameborder="0"
     scrolling="no"></iframe> -->
-
 <?php
 
 if (isset($_POST['submit-btn'])) {
@@ -29,45 +28,24 @@ if (isset($_POST['submit-btn'])) {
             </div>
             <div class="col-md-6 col-12 align-self-center">
                 <div class="signup-cnt">
-                    <h3 class="py-2 mb-5">Sign Up</h3>
-                    <form action="register.php" method="POST">
-                        <div class="form-group">
-                            <label for="fname">First Name</label>
-                            <input type="text" class="form-control" name='fname' id="fname" placeholder="Purvi"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <label for="lname">Last Name</label>
-                            <input type="text" class="form-control" name='lname' id="lname" placeholder="Harniya"
-                                required>
-                        </div>
+                    <h3 class="py-2 mb-5">Sign In</h3>
+                    <form action="login.php" method="POST">
+
                         <div class="form-group">
                             <label for="uname">User Name</label>
                             <input type="text" class="form-control" name='uname' id="uname" placeholder="pjh" required>
                         </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" name='email' id="email"
-                                placeholder="purvi@gmail.com" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="cemail">Confirm Email</label>
-                            <input type="email" class="form-control" name='cemail' id="cemail"
-                                placeholder="purvi@gmail.com" required>
-                        </div>
+
                         <div class="form-group">
                             <label for="password">Password</label>
                             <input type="password" class="form-control" name='password' id="password" required>
                         </div>
-                        <div class="form-group">
-                            <label for="confirmpass">Confirm Password</label>
-                            <input type="password" class="form-control" name='confirmpass' id="confirmpass" required>
-                        </div>
+
                         <div class="form-group">
                             <input type="submit" name="submit-btn" class="btn btn-primary" value="Sign Up">
                         </div>
                         <div class="form-group">
-                            <a href="login.php"> Have an account? Sign In</a>
+                            <a href="register.php"> Don't have an account? Sign Up</a>
                         </div>
                     </form>
                 </div>
